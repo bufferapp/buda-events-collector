0211ce219a3782a0c1601609930aeda380954e4a
David Gasquez <davidgasquez@gmail.com>
2017-07-10 13:08:53 +0200

    Cleanup

--- lambda/functions/buffer-app-events-to-firehose/config.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-# Add vendor directory to module paths
-parent_dir = os.path.abspath(os.path.dirname(__file__))
-vendor_dir = os.path.join(parent_dir, 'lib')
-sys.path.append(vendor_dir)
